88336b15e8ff4a4ac7846c668076d16bc99d0eab
Kimyen <kimyen0816@gmail.com>
2017-10-03 16:18:42 -0700

    add script to update version for synapser

---
+++ update-version-synapser.sh
@@ -0,0 +1,54 @@
+# remove the last build clone
+set +e
+rm -R ${REPO_NAME}
+set -e
+
+# clone/pull the github repo
+git clone https://github.com/${GITHUB_ACCOUNT}/${REPO_NAME}.git
+# https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/
+
+cd ${REPO_NAME}
+
+git remote add upstream https://${USERNAME}:${GITHUB_TOKEN}@github.com/${GITHUB_ACCOUNT}/${REPO_NAME}.git
+git config user.name "${USERNAME}"
+git config user.email "${USER_EMAIL}"
+
+git fetch upstream
+git checkout -b ${BRANCH} upstream/${BRANCH}
+
+# replace DESCRIPTION with $VERSION
+VERSION_LINE=`grep Version DESCRIPTION`
+sed "s|$VERSION_LINE|Version: $VERSION|g" DESCRIPTION > DESCRIPTION.temp
+
+# replace DESCRIPTION with $DATE
+DATE=`date +%Y-%m-%d`
+DATE_LINE=`grep Date DESCRIPTION.temp`
+sed "s|$DATE_LINE|Date: $DATE|g" DESCRIPTION.temp > DESCRIPTION2.temp
+
+rm DESCRIPTION
+mv DESCRIPTION2.temp DESCRIPTION
+rm DESCRIPTION.temp
+
+# replace man/synapser-package.Rd with $VERSION
+VERSION_LINE=`grep Version man/synapser-package.Rd`
+sed "s|$VERSION_LINE|Version: $VERSION|g" man/synapser-package.Rd > man/synapser-package.Rd.temp
+
+# replace man/synapser-package.Rd with $DATE
+DATE=`date +%Y-%m-%d`
+DATE_LINE=`grep Date man/synapser-package.Rd.temp`
+sed "s|$DATE_LINE|Date: $DATE|g" man/synapser-package.Rd.temp > man/synapser-package.Rd2.temp
+
+rm man/synapser-package.Rd
+mv man/synapser-package.Rd2.temp man/synapser-package.Rd
+rm man/synapser-package.Rd.temp
+
+git add --all
+git commit -m "Version $VERSION is succesfully built on $DATE"
+git push upstream ${BRANCH}
+
+git tag $VERSION
+git push upstream $VERSION
+
+cd ..
+rm -rf ${REPO_NAME}
+
