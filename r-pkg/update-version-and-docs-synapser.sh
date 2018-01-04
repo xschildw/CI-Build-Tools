1a528b6243a5db3670503f28750b75302c8f20de
Kimyen <kimyen0816@gmail.com>
2018-01-04 15:53:32 -0800

    since this script does not have write permission
    to "/usr/local/lib/R/site-library"

--- r-pkg/update-version-and-docs-synapser.sh
+++ r-pkg/update-version-and-docs-synapser.sh
@@ -1,5 +1,6 @@
 # This script is used for synapser_staging_deploy and synapser_prod_deploy. 
 # It checkout REPO_NAME repository, changes the version, update the docs, and push the changes back to the repository.
+# Notes: this script requires `pkgdown` package is available on the build machine.
 
 # Params
 # USERNAME -- Github user who is running this build
@@ -53,8 +54,7 @@ rm man/synapser-package.Rd
 mv man/synapser-package.Rd2.temp man/synapser-package.Rd
 rm man/synapser-package.Rd.temp
 
-R -e "install.packages('pkgdown', repos=c('http://cran.fhcrc.org'));\
-pkgdown::build_site()"
+R -e "pkgdown::build_site()"
 
 git add --all
 git commit -m "Version $VERSION is succesfully built on $DATE"
