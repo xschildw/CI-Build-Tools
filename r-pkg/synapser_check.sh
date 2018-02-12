379ccacc72e38f75e86ce9a00a82397e3b860a9c
Kimyen <kimyen0816@gmail.com>
2018-02-12 15:13:39 -0800

    fix syntax error

--- r-pkg/synapser_check.sh
+++ r-pkg/synapser_check.sh
@@ -6,9 +6,9 @@ export ATTEMPTS=5
 export FOUND=False
 while [ $ATTEMPTS -gt 0 ]
 do
-  if [ $(curl -v --silent ${RAN}/src/contrib/PACKAGES 2>&1 | grep "Package: synapser" -A 2 | grep "Version: ${VERSION_TO_CHECK}" | wc -c) -eq 0 ]
+  if [ $( curl -v --silent ${RAN}/src/contrib/PACKAGES 2>&1 | grep "Package: synapser" -A 2 | grep "Version: ${VERSION_TO_CHECK}" | wc -c ) -eq 0 ]
   then
-    sleep(10)
+    sleep 10
     ATTEMPTS=$(( $ATTEMPTS - 1 ))
   else
     FOUND=TRUE
