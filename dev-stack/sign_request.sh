09b195f639ec4df621a50643a5bab17ad1693c83
Kimyen <kimyen0816@gmail.com>
2018-09-07 11:55:23 -0700

    do not include new line chars

--- dev-stack/sign_request.sh
+++ dev-stack/sign_request.sh
@@ -15,17 +15,15 @@ protocol="$(echo $URL | grep :// | sed -e's,^\(.*://\).*,\1,g')"
 # remove the protocol from url
 url="$(echo ${URL/$protocol/})" 
 path=/$(echo $url | grep / | cut -d/ -f2-)
-echo $path
 
 sig_timestamp=$(date -u +"%Y-%m-%dT%H:%M:%S.000Z")
 sig_data="$USER_ID$path$sig_timestamp"
-echo $sig_data
 
-decoded_key=$(echo $APIKEY | base64 --decode)
+# echo -n to not include new line character
+decoded_key=$(echo -n $APIKEY | base64 --decode)
 
 signature_raw=$(echo -n "$sig_data" | iconv -t UTF8 | openssl dgst -sha1 -hmac "$decoded_key")
 prefix_to_remove="(stdin)= " 
-signature="$(echo ${signature_raw#$prefix_to_remove} | base64)" 
-echo $signatures
+signature="$(echo -n ${signature_raw#$prefix_to_remove} | base64)" 
 
 echo -H \"userId:$USER_ID\" -H \"signatureTimestamp:$sig_timestamp\" -H \"signature:$signature\"
