0ac9b0b1481e8d76ab9f2cb147c777cf58cce279
Kimyen <kimyen0816@gmail.com>
2018-09-12 17:37:32 -0700

    add more debug

--- dev-stack/create_user.sh
+++ dev-stack/create_user.sh
@@ -27,12 +27,14 @@ new_user=$(echo curl -X POST -H \"Accept:application/json\" -H \"Content-Type:ap
 prefix="{\"id\":\""
 suffix="\"}"
 id=$(echo $new_user | sed -e "s/^$prefix//" -e "s/$suffix$//")
+echo "Created user with ID: $id"
 
 ## Step 3 -- Add the test user to Certified user group
 # PUT /user/{id}/certificationStatus
 echo "Add the test user to Certified user group"
 url=$REPO_ENDPOINT/repo/v1/user/$id/certificationStatus?isCertified=True
 signed_headers=$(curl -s https://raw.githubusercontent.com/kimyen/CI-Build-Tools/PLFM-5028/dev-stack/sign_request.sh | bash -s $url $ADMIN_USERNAME $ADMIN_APIKEY)
+echo curl -X PUT -H \"Accept:application/json\" -H \"Content-Type:application/json\" $signed_headers \"$url\"
 echo curl -X PUT -H \"Accept:application/json\" -H \"Content-Type:application/json\" $signed_headers \"$url\" | bash
 
 ## Step 4 -- Verified that the new user can login using username and password
