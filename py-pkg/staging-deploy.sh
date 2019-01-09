424f60786c35c0d6db72572a1574d05684853540
KimYen Ladia <kimyen0816@gmail.com>
2019-01-09 14:12:57 -0800

    SYNPY-618 use release candidate branch as version
    #comment continue working on the script
    #start-progress

--- py-pkg/staging-deploy.sh
+++ py-pkg/staging-deploy.sh
@@ -31,7 +31,8 @@ cd ${REPO_NAME}
 git config user.name "${GITHUB_USERNAME}"
 git config user.email "${EMAIL}"
 
-export VERSION=`echo $(echo ${GIT_BRANCH}.$BUILD_NUMBER | sed 's/origin\/v//g; s/-rc//g')`
+# export VERSION=`echo $(echo ${GIT_BRANCH}.$BUILD_NUMBER | sed 's/origin\/v//g; s/-rc//g')`
+export VERSION=${GIT_BRANCH}
 git checkout ${GIT_BRANCH}
 
 # update version
