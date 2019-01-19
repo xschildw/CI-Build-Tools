c74fd9531f84d9f90e1fa14ca454173793866a01
KimYen Ladia <kimyen0816@gmail.com>
2019-01-18 18:28:44 -0800

    refactor staging_deploy.sh to deploy.sh
    to be reused for both staging and prod

---
+++ py-pkg/deploy.sh
@@ -0,0 +1,59 @@
+# This script, when run from Jenkins, releases the python package to PYPI_REPO.
+
+# Parameters
+# GITHUB_USERNAME -- The Github user who is running this build
+# GITHUB_ACCOUNT -- The target Github account
+# GITHUB_TOKEN -- The Github token that grants access to GITHUB_ACCOUNT for GITHUB_USERNAME
+# EMAIL -- The email of the user in GITHUB_USERNAME
+# REPO_NAME -- The Github repo to get the Python code from
+# GIT_BRANCH -- The release candidate branch
+# PYPI_REPO -- Either test.pypi.org or pypi.org
+# PYPI_USERNAME -- The PYPI_REPO user
+# PYPI_PASSWORD -- The PYPI_REPO user's password
+
+# remove the last build clone
+set +e
+rm -R ${REPO_NAME}
+set -e
+
+# create .pypirc file
+echo [distutils] > ~/.pypirc
+echo index-servers=PYPI >> ~/.pypirc
+echo >> ~/.pypirc
+echo [PYPI] >> ~/.pypirc
+echo repository: https://$PYPI_REPO/legacy/ >> ~/.pypirc
+echo username:$PYPI_USERNAME >> ~/.pypirc
+echo password:$PYPI_PASSWORD >> ~/.pypirc
+
+# clone the Github repo
+git clone https://github.com/${GITHUB_ACCOUNT}/${REPO_NAME}.git
+
+cd ${REPO_NAME}
+git config user.name "${GITHUB_USERNAME}"
+git config user.email "${EMAIL}"
+
+export VERSION=`echo $(echo ${GIT_BRANCH}.$BUILD_NUMBER | sed 's/origin\/v//g; s/-rc//g')`
+git checkout ${GIT_BRANCH}
+
+# update version
+sed "s|\"latestVersion\":.*$|\"latestVersion\":\"$VERSION\",|g" synapseclient/synapsePythonClient > temp
+rm synapseclient/synapsePythonClient
+mv temp synapseclient/synapsePythonClient
+
+# ensure environment
+python3 -m pip install twine==1.11.0 setuptools==38.6.0
+
+# install synapseclient
+python3 setup.py install
+
+# create distribution
+python3 setup.py sdist
+
+# upload to PYPI 
+twine upload --repository PYPI dist/*
+
+# clean up
+cd ..
+rm -rf ${REPO_NAME}
+
+
